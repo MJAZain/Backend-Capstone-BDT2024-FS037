9b9b36aafeee6a75fe0ba746727dc06e3d9f89af
MJAZain <mjibrilabdullahz1@gmail.com>
2024-07-18 18:44:24 +0700

    .

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const verifyToken = require('./middleware/authMiddleware'); // Import your auth middleware
+const verifyToken = require('./middleware/authMiddleware');
 
 dotenv.config();
 
 const authRoutes = require('./routes/auth');
 const protectedRoutes = require('./routes/protected');
 const forgotPasswordRoutes = require('./routes/forgot');
-const orderRoutes = require('./routes/order'); // Import order routes (now named order.js)
+const orderRoutes = require('./routes/order');
+const tourRoutes = require('./routes/tour');
+const orderTourRoutes = require('./routes/orderTour');
+const getTourRoutes = require('./routes/getTour');
 
 const app = express();
 
@@ -22,21 +25,29 @@ mongoose.connect(process.env.MONGO_URI, {
 })
   .then(() => console.log('MongoDB connected'))
   .catch(err => {
-    console.error('MongoDB connection error:', err); // Debug log
-    process.exit(1); // Exit process with failure
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
   });
 
 // Public Routes
 app.use('/api/auth', authRoutes);
 app.use('/api', forgotPasswordRoutes);
+app.use('/api/tours', tourRoutes);
+
+// Protected Routes
+app.use('/api/order-tours', verifyToken, (req, res, next) => {
+  console.log('Reached /api/order-tours route in server.js');
+  next();
+}, orderTourRoutes);
 app.use('/api/protected', verifyToken, protectedRoutes);
 app.use('/api/orders', verifyToken, orderRoutes);
+app.use('/api/get-tour-orders', verifyToken, getTourRoutes); // Add this line for displaying tour orders
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  console.error('Error:', err.stack); // Debug log
+  console.error('Error:', err.stack);
   res.status(500).send('Something broke!');
 });
 
-const PORT = process.env.PORT || 5000; // Ensure this port matches your front-end expectations
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
